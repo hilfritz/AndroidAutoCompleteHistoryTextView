c2d0ae727ea2f3eb3daadb9bf257e4e7c2680b70
Hilfritz <hilfritz@users.noreply.github.com>
2025-08-14 16:42:07 -0400

    updated library dependencies

--- app/src/main/java/com/hilfritz/autocompletehistorytextview/MainActivity.java
+++ app/src/main/java/com/hilfritz/autocompletehistorytextview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hilfritz.autocompletehistorytextview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import kotlinx.coroutines.*;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Button saveBtn;
     Button resetBtn;
     AutoCompleteHistoryTextView name, age, email;
-
+    ConstraintLayout root;
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,15 @@ public class MainActivity extends AppCompatActivity {
         name = (AutoCompleteHistoryTextView) findViewById(R.id.name);
         age = (AutoCompleteHistoryTextView) findViewById(R.id.age);
         email = (AutoCompleteHistoryTextView) findViewById(R.id.email);
-
+        root = findViewById(R.id.constraint_layout);
+        // Replace MyCustomView.class with your actual custom view class
+        root.setOnClickListener(view -> {
+            clearFocus();
+        });
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "saveBtn: onClick: ");
                 if (name.isEmptyInput()){
                     toast("Please provide name.");
                     return;
@@ -41,21 +51,51 @@ public class MainActivity extends AppCompatActivity {
                     toast("Please provide age.");
                     return;
                 }
+                Log.d(TAG, "saveBtn: onClick: Field inputs okay");
                 name.saveToHistory(name.getText().toString());
                 email.saveToHistory(email.getText().toString());
                 age.saveToHistory(age.getText().toString());
+                Log.d(TAG, "saveBtn: onClick: Field inputs saved.");
+                clearFocus();
+
                 toast("Saved!");
             }
         });
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "resetBtn: onClick: ");
+                clearFocus();
+                clearFieldInputs();
                 name.reset();
                 email.reset();
                 age.reset();
                 toast("Reset done!");
             }
         });
+
+    }
+
+    private void clearFieldInputs() {
+        root.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "run: delay: clearFieldInputs: ");
+                name.setText("");
+                email.setText("");
+                age.setText("");
+            }
+        }, 1500);
+
+    }
+
+    private void clearFocus() {
+        FocusUtils.clearFocusFromCustomViews(
+                root,
+                AutoCompleteHistoryTextView.class,
+                /* clearAnyFocusedView = */ true,
+                /* hideKeyboard = */ true
+        );
     }
 
     public void toast(String str){
